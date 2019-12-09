d6b54463ce55904908a22cfc47027868ea23de5e
Paulo Morais <phm.paulomorais@gmail.com>
2019-12-09 19:04:02 -0300

    Corrigida propriedade css

--- src/styles/global.js
+++ src/styles/global.js
@@ -14,7 +14,7 @@ export default createGlobalStyle`
 
   body {
     background: #7159c1;
-    -webkit-font-smoothing: antialised !important;
+    -webkit-font-smoothing: antialiased !important;
   }
 
   body, input, button {
